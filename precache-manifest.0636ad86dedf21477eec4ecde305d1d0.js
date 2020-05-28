9678cfacbd94cfafbf28552868f9b7d8d99726b9
mahimshridhar <mahimshridhar@gmail.com>
2020-05-28 21:54:51 +0530

    Updates

---
+++ precache-manifest.0636ad86dedf21477eec4ecde305d1d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f684714a112eaf2b5ec719801abdf6db",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "45f118bcd1c5a144018a",
+    "url": "/assignment/static/css/main.9b1b5bc9.chunk.css"
+  },
+  {
+    "revision": "7f9f528d97c2047287b9",
+    "url": "/assignment/static/js/2.b8caadba.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/assignment/static/js/2.b8caadba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f118bcd1c5a144018a",
+    "url": "/assignment/static/js/main.aa8cd26d.chunk.js"
+  },
+  {
+    "revision": "b883d490c52c7475133c",
+    "url": "/assignment/static/js/runtime-main.bb53ba0a.js"
+  }
+]);
